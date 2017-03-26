c0ea4a580c1e389e4ff9d3b75ce2db25fd346e20
Bo Zhang <zannb@outlook.com>
2017-03-26 17:43:31 +0800

    add docopt

--- robet_up_web.py
+++ robet_up_web.py
@@ -1,3 +1,17 @@
+"""robet_up_web
+
+Usage:
+    robet_up_web.py -g <website> <roomid>
+    robet_up_web.py -h
+
+Option:
+  -h --help     Show this
+  -g --goto     goto
+"""
+
+
+from docopt import docopt
+
 import json
 import time
 
@@ -49,25 +63,27 @@ class RobetUpWeb:
 
 
 if __name__ == "__main__":
-    import os
-    path=os.path.expanduser('~/IdeaProjects/qb/')
-    d=RobetUpWeb(path+'douyu_cookie.txt')
-
-    # d.go_to_website('http://weibo.com')
+    arguments= docopt(__doc__)
+    print(arguments)
+    # import os
+    # path=os.path.expanduser('~/IdeaProjects/qb/')
+    # d=RobetUpWeb(path+'douyu_cookie.txt')
+    #
+    # # d.go_to_website('http://weibo.com')
+    # # d.login()
+    # # time.sleep(5)
+    # # d.login()
+    # # time.sleep(3)
+    # # d.login()
+    # # d.search_in_weibo('just_a_sleep')
+    # d.browser.get('http://www.douyu.com/t/lck')
     # d.login()
-    # time.sleep(5)
-    # d.login()
+    # # 关闭弹出来的小页面
+    # n_close=d.browser.find_element_by_class_name('normallevel-close')
     # time.sleep(3)
-    # d.login()
-    # d.search_in_weibo('just_a_sleep')
-    d.browser.get('http://www.douyu.com/t/lck')
-    d.login()
-    # 关闭弹出来的小页面
-    n_close=d.browser.find_element_by_class_name('normallevel-close')
-    time.sleep(3)
-    ActionChains(d.browser).move_to_element(n_close).click(n_close).perform()
-    time.sleep(5)
-    d.get_danmu()
+    # ActionChains(d.browser).move_to_element(n_close).click(n_close).perform()
+    # time.sleep(5)
+    # d.get_danmu()
 
 
     # text_area=browser.find_element_by_class_name('s_ipt')
