207b710cf7d5be37c9761ae9f4ad5f23f1ac6355
Bo <zannb@outlook.com>
2017-04-13 16:54:39 +0800

    在发送的信息之后要加上\r\n才能正常的工作，不知道是什么原因。。。

--- seriel_learn.py
+++ seriel_learn.py
@@ -1,7 +1,42 @@
-import serial
 import serial.tools.list_ports
+import serial
+import time
+from threading import Thread
 
-plist=serial.tools.list_ports.comports()
-print(1)
-for each in plist:
-    print(each)
+port_list = serial.tools.list_ports.comports()
+port_list = [each.device for each in port_list]
+print(port_list)
+serial1 = serial.Serial(
+    port='com9',
+    baudrate=9600
+)
+
+
+# print(serial1)
+
+# serial1.open()
+
+def rx():
+    while 1:
+        time.sleep(0.1)
+        n = serial1.inWaiting()
+        if n > 0:
+            print(n)
+            data = serial1.read(n)
+            print(data)
+
+
+def rx_thread():
+    t = Thread(name="rx", target=rx)
+    t.start()
+
+
+cnt = 0
+rx_thread()
+while 1:
+    s=input()
+    if len(s)>0:
+        # 不知道为什么这里要加上\r\n才能正常地返回
+        s+='\r\n'
+        data=s.encode()
+        print(serial1.write(data))
